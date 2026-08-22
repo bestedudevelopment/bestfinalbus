783c4e451b407f591459b168e3ad44ef8ceef30e
bestedudevelopment <admin.svtbest@gmail.com>
2026-08-22 10:38:31 +0530

    Create firebase.js

---
+++ core/firebase.js
@@ -0,0 +1,67 @@
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+
+const firebaseConfig = {
+
+    apiKey:
+        "AIzaSyCvpd7HxpLq4IJ22cMTldV3sanz35tt3H",
+
+    authDomain:
+        "best----bus-application.firebaseapp.com",
+
+    projectId:
+        "best----bus-application",
+
+    storageBucket:
+        "best----bus-application.firebasestorage.app",
+
+    messagingSenderId:
+        "428353631166",
+
+    appId:
+        "1:428353631166:web:e58e03cb28a52a790e3fef",
+
+    measurementId:
+        "G-5XP2LE6FH1"
+
+};
+
+
+const app =
+    initializeApp(
+        firebaseConfig
+    );
+
+
+const auth =
+    getAuth(app);
+
+
+const db =
+    getFirestore(app);
+
+
+const storage =
+    getStorage(app);
+
+
+export {
+    app,
+    auth,
+    db,
+    storage
+};
