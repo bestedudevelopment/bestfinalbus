f472fb8e086e26ab8aa0b1728563be40b084e38d
bestedudevelopment <admin.svtbest@gmail.com>
2026-08-22 11:01:06 +0530

    Update firebase.js

--- core/firebase.js
+++ core/firebase.js
@@ -15,14 +15,15 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCvpd7HxpLq4IJ22cMTldV3sanz35tt3H8",
-    authDomain: "best----bus-application.firebaseapp.com",
-    projectId: "best----bus-application",
-    storageBucket: "best----bus-application.firebasestorage.app",
-    messagingSenderId: "428353631166",
-    appId: "1:428353631166:web:e58e03cb28a52a790e3fef",
-    measurementId: "G-5XP2LE6FH1"
+  apiKey: "AIzaSyCvpd7HxpLq4IJ22cMTldV3sanz35tt3H8",
+  authDomain: "best----bus-application.firebaseapp.com",
+  projectId: "best----bus-application",
+  storageBucket: "best----bus-application.firebasestorage.app",
+  messagingSenderId: "428353631166",
+  appId: "1:428353631166:web:e58e03cb28a52a790e3fef",
+  measurementId: "G-5XP2LE6FH1"
 };
 
 
